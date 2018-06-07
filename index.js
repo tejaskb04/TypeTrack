38a5b4e1d5f2f87bf5c7a63b409f0de09f4f1486
Tejas Bharadwaj <tejaskb@uw.edu>
2018-06-06 23:14:31 -0700

    Setup, Fetched Quotes

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = pageLoad;
+
+function pageLoad() {
+    console.log("js linked");
+    getQuotes();
+}
+
+// checks status of ajax calls
+function checkStatus(response) {  
+    if (response.status >= 200 && response.status < 300) {  
+        return response.text();
+    } else {  
+        return Promise.reject(new Error(response.status + ": " + response.statusText)); 
+    } 
+}
+
+// gets quotes from api
+function getQuotes() {
+    var url = "https://cors.io/?https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+    fetch(url)
+        .then(checkStatus)
+        .then(JSON.parse)
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
